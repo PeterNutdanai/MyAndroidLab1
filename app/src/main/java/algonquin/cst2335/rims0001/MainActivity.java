5101da345c9c1c64491716ac5d425c348822c530
Nutdanai <rims0001@algonquinlive.com>
2023-06-08 15:48:27 -0400

    Week 4: Fix the error

--- app/src/main/java/algonquin/cst2335/rims0001/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/rims0001/MainActivity.java
@@ -18,13 +18,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
+import algonquin.cst2335.rims0001.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private static String TAG = "MainActivity";
 
     protected ActivityMainBinding binding;
-    private ImageView profileImage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,9 +35,8 @@ public class MainActivity extends AppCompatActivity {
         Log.w(TAG, "In onCreate() - Loading Widgets");
 
         Log.d(TAG, "Message");
-//        binding = ActivityMainBinding.inflate(getLayoutInflater());
-//        setContentView(binding.getRoot());
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         binding.loginButton.setOnClickListener( clk -> {
             Log.d(TAG, "You clicked the button");
@@ -51,74 +52,40 @@ public class MainActivity extends AppCompatActivity {
             nextPage.putExtra("DAY","Sunday");
 
             startActivity(nextPage);
-
-
-        Intent call = new Intent(Intent.ACTION_DIAL);
-        call.setData(Uri.parse("tel: " + "3435585543"));
-
-        startActivity(call);
-
-        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        ActivityResultLauncher<Intent> camaraResult = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback<ActivityResult>() {
-                    @Override
-                    public void onActivityResult(ActivityResult result) {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                            if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
-                                startActivity(cameraIntent);
-                            else
-                                requestPermissions(new String[] {Manifest.permission.CAMERA}, 20);
-
-                            Intent data = result.getData();
-                            Bitmap thumbnail = data.getParcelableExtra("data");
-                            profileImage.setImageBitmap( thumbnail );
-                        }
-                    }
-                });
-                camaraResult.launch(cameraIntent);
-
     }
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-//
-//        if((requestCode == 20) && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-//            startActivity( new Intent(MediaStore.ACTION_IMAGE_CAPTURE) );
-//    }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.w("MainActivity","In onCreate() - Loading Widgets");
+        Log.w(TAG,"In onCreate() - Loading Widgets");
 
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.w("MainActivity","In onCreate() - Loading Widgets");
+        Log.w(TAG,"In onCreate() - Loading Widgets");
 
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.w("MainActivity","In onCreate() - Loading Widgets");
+        Log.w(TAG,"In onCreate() - Loading Widgets");
 
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.w("MainActivity","In onCreate() - Loading Widgets");
+        Log.w(TAG,"In onCreate() - Loading Widgets");
 
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.w("MainActivity","In onCreate() - Loading Widgets");
+        Log.w(TAG,"In onCreate() - Loading Widgets");
 
     }
 }
--- app/src/main/java/algonquin/cst2335/rims0001/SecondActivity.java
+++ app/src/main/java/algonquin/cst2335/rims0001/SecondActivity.java
@@ -1,19 +1,35 @@
 package algonquin.cst2335.rims0001;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.ImageView;
+
+import algonquin.cst2335.rims0001.databinding.ActivityMainBinding;
+import algonquin.cst2335.rims0001.databinding.ActivitySecondBinding;
 
 public class SecondActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
+    private ActivitySecondBinding binding;
+    private ImageView profileImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivitySecondBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         Intent fromPrevious = getIntent();
@@ -31,7 +47,30 @@ public class SecondActivity extends AppCompatActivity {
             finish();
         });
 
-        setContentView(R.layout.activity_second);
+        Intent call = new Intent(Intent.ACTION_DIAL);
+        call.setData(Uri.parse("tel: " + "3435585543"));
+
+        startActivity(call);
+
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        ActivityResultLauncher<Intent> camaraResult = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
+                                startActivity(cameraIntent);
+                            else
+                                requestPermissions(new String[] {Manifest.permission.CAMERA}, 20);
+
+                            Intent data = result.getData();
+                            Bitmap thumbnail = data.getParcelableExtra("data");
+                            profileImage.setImageBitmap( thumbnail );
+                        }
+                    }
+                });
+        camaraResult.launch(cameraIntent);
 
     }
 }
