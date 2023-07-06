f15ee875d897231c66fa54c754fc30ca6871af84
Nutdanai <rims0001@algonquinlive.com>
2023-07-06 09:55:22 -0400

    Week6 Created ChatRoomViewModel

---
+++ app/src/main/java/algonquin/cst2335/rims0001/data/ChatRoomViewModel.java
@@ -0,0 +1,9 @@
+package algonquin.cst2335.rims0001.data;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+
+public class ChatRoomViewModel extends ViewModel {
+    public MutableLiveData<ArrayList<String>> messages = new MutableLiveData< >();}
--- app/src/main/java/algonquin/cst2335/rims0001/data/MainActivityViewModel.java
+++
@@ -1,9 +0,0 @@
-package algonquin.cst2335.rims0001.data;
-
-import androidx.lifecycle.ViewModel;
-
-import java.util.ArrayList;
-
-public class MainActivityViewModel extends ViewModel {
-    protected ArrayList<String> messageText = new ArrayList<>();
-}
