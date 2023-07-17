0af887c38174a077379d98b6686c9504170f2776
Peter Nutdanai <rims0001@algonquinlive.com>
2023-07-17 15:16:59 -0400

    Finished MessageDetailsFragment

--- app/src/main/java/algonquin/cst2335/rims0001/data/MessageDetailsFragment.java
+++ app/src/main/java/algonquin/cst2335/rims0001/data/MessageDetailsFragment.java
@@ -1,7 +1,18 @@
 package algonquin.cst2335.rims0001.data;
 
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
 
 public class MessageDetailsFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
 }
