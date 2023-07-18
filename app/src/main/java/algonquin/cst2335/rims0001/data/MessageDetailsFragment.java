a4ca65e9ecf7f83deab34c60bedfb0cd028ff49c
Peter Nutdanai <rims0001@algonquinlive.com>
2023-07-18 09:13:38 -0400

    Week 8 Created constructor in MessageDetailsFragment

--- app/src/main/java/algonquin/cst2335/rims0001/data/MessageDetailsFragment.java
+++ app/src/main/java/algonquin/cst2335/rims0001/data/MessageDetailsFragment.java
@@ -8,11 +8,25 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import algonquin.cst2335.rims0001.databinding.DetailsLayoutBinding;
+
 public class MessageDetailsFragment extends Fragment {
+    ChatMessage selected;
+
+    public MessageDetailsFragment(ChatMessage m){
+        selected = m;
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        super.onCreateView(inflater, container, savedInstanceState);
+        DetailsLayoutBinding binding = DetailsLayoutBinding.inflate(inflater);
 
-        return super.onCreateView(inflater, container, savedInstanceState);
+        binding.messageText.setText(selected.message);
+        binding.timeText.setText(selected.timeSent);
+        binding.databaseText.setText("Id = " + selected.id);
+
+        return binding.getRoot();
+
     }
 
 }
